6724c71f2eb965120a7e9a828c9fa7731e313ec6
trick77 <trick77@users.noreply.github.com>
2016-11-26 22:17:16 +0100

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -21,14 +21,15 @@
 /sbin/iptables -A SIP -p udp -m udp --dport 5060 -m string --string "sip-scan" --algo bm --to 1500 -j DROP
 /sbin/iptables -A SIP -p udp -m udp --dport 5060 -m string --string "eyeBeam" --algo bm --to 1500 -j DROP
 /sbin/iptables -A SIP -p udp -m udp --dport 5060 -m string --string "VaxSIPUserAgent" --algo bm --to 1500 -j DROP
+/sbin/iptables -A SIP -p udp -m udp --dport 5060 -m string --string "sip:nm@nm" --algo bm --to 1500 -j DROP
+/sbin/iptables -A SIP -p udp -m udp --dport 5060 -m string --string "sip:nm2@nm2" --algo bm --to 1500 -j DROP
+/sbin/iptables -A SIP -p udp -m udp --dport 5060 -m string --string "sip:n@n" --algo bm --to 1500 -j DROP
 
 # Rate limit INVITE and REGISTER messages
 /sbin/iptables -A SIP -p udp -m udp --dport 5060 -m string --string "INVITE" --algo bm --from 23 --to 28 -j SIPINV
 /sbin/iptables -A SIP -p udp -m udp --dport 5060 -m string --string "REGISTER" --algo bm --from 23 --to 30 -j SIPREG
+/sbin/iptables -A SIP -p udp -m udp --dport 5060 -m string --string "OPTIONS" --algo bm --from 23 --to 30 -j SIPOPT
 /sbin/iptables -A SIP -j ACCEPT
-
-/sbin/iptables -A SIP -p udp -m udp --dport 5060 -m string --string "INVITE" --algo bm --from 23 --to 28 -j SIPINV
-/sbin/iptables -A SIP -p udp -m udp --dport 5060 -m string --string "REGISTER" --algo bm --from 23 --to 30 -j SIPREG
 
 # The limits of these rules may have to be changed. While testing, it's best to disable them.
 /sbin/iptables -A SIPINV -m hashlimit --hashlimit-upto 4/min --hashlimit-burst 4 --hashlimit-mode srcip,dstip,dstport --hashlimit-name sip-rateinv --hashlimit-srcmask 24 -j ACCEPT
@@ -39,3 +40,6 @@
 /sbin/iptables -A SIPREG -m limit --limit 10/min -j LOG --log-prefix "SIPREG DROP: "
 /sbin/iptables -A SIPREG -j REJECT
 
+/sbin/iptables -A SIPOPT -m hashlimit --hashlimit-upto 6/hour --hashlimit-burst 6 --hashlimit-mode srcip,dstip,dstport --hashlimit-name sip-rateopt --hashlimit-srcmask 24 -j ACCEPT
+/sbin/iptables -A SIPOPT -m limit --limit 10/min -j LOG --log-prefix "SIPOPT DROP: "
+/sbin/iptables -A SIPOPT -j REJECT
